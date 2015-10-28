a027b3afb790cea7eac594ed8874ddba085baba0
ellenschwartau <ellen.schwartau@hotmail.de>
2015-10-28 08:15:22 +0100

    [ueb02]
    + initiale Dateien

---
+++ src/de/fhwedel/kdbt/ueb02/Car.java
@@ -0,0 +1,25 @@
+package de.fhwedel.kdbt.ueb02;
+
+public class Car {
+
+    private String model;
+    private Pilot pilot;
+
+    public Car(String model, Pilot pilot) {
+        this.model = model;
+        this.pilot = pilot;
+    }
+
+    public Car(String model) {
+        this(model, null);
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Pilot getPilot() {
+        return pilot;
+    }
+
+}
